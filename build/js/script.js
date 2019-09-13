0c799c0325f67d817db4932e533081cbe6ebb29b
Bogdan <dastan_krimea@mail.ru>
2019-09-14 01:14:19 +0300

    codereview part 2

--- build/js/script.js
+++ build/js/script.js
@@ -1,4 +1,66 @@
 'use strict';
 (function () {
 
+  // скроллиги
+  (function () {
+
+    var KEYCODE_ENTER = 13;
+    var servicesBlock = document.querySelector('.services');
+    var contactsBlock = document.querySelector('.footer');
+    var scrollButtonService = document.querySelector('.main-screen__services');
+    var scrollButtonContacts = document.querySelector('.main-screen__contacts');
+    var scrollButtoncontacts2 = document.querySelector('.header__scroll');
+
+    var scrolling = function (scrollItem, scrollToBlock) {
+      scrollItem.addEventListener('keydown', function (evt) {
+        if (evt.keyCode === KEYCODE_ENTER) {
+          evt.preventDefault();
+          scrollToBlock.scrollIntoView({block: 'start', behavior: 'smooth'});
+        }
+      });
+
+      scrollItem.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        scrollToBlock.scrollIntoView({block: 'start', behavior: 'smooth'});
+      });
+    };
+    scrolling(scrollButtonService, servicesBlock);
+    scrolling(scrollButtonContacts, contactsBlock);
+    scrolling(scrollButtoncontacts2, contactsBlock);
+  })();
+
+  // главное меню в хедере
+  (function () {
+    var burgerButton = document.querySelector('.header__burger');
+    var navBlock = document.querySelector('.header__nav');
+    burgerButton.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      navBlock.classList.toggle('header__nav--active');
+    });
+  })();
+
+  // аккордеоны в сервисах
+  (function () {
+    var clickableItems = document.querySelectorAll('.services__main-item');
+    var container = document.querySelector('.services__main-list');
+
+    clickableItems.forEach(function (element) {
+      element.addEventListener('click', function (evt) {
+        container.style = '';
+        evt.preventDefault();
+        var activeElement = document.querySelector('.services__main-item--active');
+        var media = window.matchMedia('(max-width: 767px)').matches;
+
+        if (media) {
+          element.classList.toggle('services__main-item--active');
+        } else {
+          activeElement.classList.remove('services__main-item--active');
+          element.classList.add('services__main-item--active');
+        }
+
+      container.style = 'height: ' + element.clientHeight + 'px;';
+      });
+    });
+  })();
+
 })();
--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/img/*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
   gulp.watch("source/js/**/*.js", gulp.series("copy", "refresh"));
 });
@@ -68,7 +68,7 @@ gulp.task("webp", function () {
 });
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/{icon-*,htmlacademy*}.svg")
+  return gulp.src("source/img/*.svg")
     .pipe(svgstore({inlineSvg: true}))
     .pipe(rename("sprite_auto.svg"))
     .pipe(gulp.dest("build/img"));
@@ -86,6 +86,7 @@ gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
+    "source/css/**",
     "source/js/**",
     "source//*.ico"
     ], {
